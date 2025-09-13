749a5bc20824f325e6cc8bd3140bd1e9128e695a
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-09-13 23:44:03 +0000

    Script that prints two arguments passed to it, in the following format: “ is ”

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,5 @@
-// Check if exactly two arguments are provided
-if (process.argv.length === 4) {
-  // process.argv[0] is 'node', process.argv[1] is the script path
-  const arg1 = process.argv[2];
-  const arg2 = process.argv[3];
-  console.log(`${arg1} is ${arg2}`);
-} else {
-  console.log("HBTN is undefined.");
-}
+// script3.js
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(`${arg1} is ${arg2}`);
