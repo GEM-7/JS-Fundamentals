6408db6fd34b1687dbe996087c9fdb2f38aacf2e
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-09-13 23:40:52 +0000

    Script that prints two arguments passed to it, in the following format: “ is ”

--- 4-concat.js
+++ 4-concat.js
@@ -5,5 +5,5 @@ if (process.argv.length === 4) {
   const arg2 = process.argv[3];
   console.log(`${arg1} is ${arg2}`);
 } else {
-  console.log("Please provide exactly two arguments.");
+  console.log("undefined is undefined.");
 }
