8055198d4491843e8f5049f7b832ec78c4b7f637
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-09-13 23:38:52 +0000

    Script that prints two arguments passed to it, in the following format: “ is ”

---
+++ 4-concat.js
@@ -0,0 +1,9 @@
+// Check if exactly two arguments are provided
+if (process.argv.length === 4) {
+  // process.argv[0] is 'node', process.argv[1] is the script path
+  const arg1 = process.argv[2];
+  const arg2 = process.argv[3];
+  console.log(`${arg1} is ${arg2}`);
+} else {
+  console.log("Please provide exactly two arguments.");
+}
